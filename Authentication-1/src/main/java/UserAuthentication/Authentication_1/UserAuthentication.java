1c70a14500321811296e8c75387859646c795a85
codergirl <64556236+priyanka7123@users.noreply.github.com>
2023-07-31 20:33:59 +0530

    Add files via upload

---
+++ Authentication-1/src/main/java/UserAuthentication/Authentication_1/UserAuthentication.java
@@ -0,0 +1,16 @@
+package UserAuthentication.Authentication_1;
+
+public class UserAuthentication {
+	public String username()
+	{
+		String email = "priyanka@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "panda@28";
+		return password;
+	}
+
+}
---
+++ Authentication-1/src/test/java/UserAuthentication/Authentication_1/TestTest.java
@@ -0,0 +1,16 @@
+package UserAuthentication.Authentication_1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestTest 
+{
+   
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
